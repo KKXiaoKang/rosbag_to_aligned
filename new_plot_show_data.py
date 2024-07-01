fc2f65fac8dfd77d5c4c12562f05b884f3263ee0
KKXiaoKang <410080663@qq.com>
2024-06-30 20:58:21 -0700

    feat:增加保存state和command比较图片的功能

--- new_plot_show_data.py
+++ new_plot_show_data.py
@@ -1,42 +1,55 @@
 import rosbag
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-# 1. 读取rosbag文件并提取所需数据
-bag = rosbag.Bag('./2024-04-26-17-58-06.bag', 'r')
-kuavo_arm_traj_data = []
-robot_arm_q_v_tau_data = []
+def use_rosbag_to_show(bag_name):
+    base_name = os.path.splitext(os.path.basename(bag_name))[0]
+    # 读取rosbag文件并提取所需数据
+    bag = rosbag.Bag(bag_name, 'r')
+    kuavo_arm_traj_data = []
+    robot_arm_q_v_tau_data = []
 
-kuavo_arm_traj_data_time_stamp=[]
-robot_arm_q_v_tau_data_time_stamp=[]
+    kuavo_arm_traj_data_time_stamp=[]
+    robot_arm_q_v_tau_data_time_stamp=[]
 
-for topic, msg, t in bag.read_messages(topics=['/kuavo_arm_traj', '/robot_arm_q_v_tau']):
-    if topic == '/kuavo_arm_traj':
-        kuavo_arm_traj_data.append(msg.position)
-        kuavo_arm_traj_data_time_stamp.append(msg.header.stamp)
-    elif topic == '/robot_arm_q_v_tau':
-        # 将弧度转换为角度
-        robot_arm_q_v_tau_data.append(np.rad2deg(msg.q))
-        robot_arm_q_v_tau_data_time_stamp.append(msg.header.stamp)
+    for topic, msg, t in bag.read_messages(topics=['/kuavo_arm_traj', '/robot_arm_q_v_tau']):
+        if topic == '/kuavo_arm_traj':
+            kuavo_arm_traj_data.append(msg.position)
+            #kuavo_arm_traj_data_time_stamp.append(msg.header.stamp)
+        elif topic == '/robot_arm_q_v_tau':
+            # 将弧度转换为角度
+            robot_arm_q_v_tau_data.append(np.rad2deg(msg.q))
+            #robot_arm_q_v_tau_data_time_stamp.append(msg.header.stamp)
 
-bag.close()
+    bag.close()
 
-print("kuavo_arm_traj_data : ", len(kuavo_arm_traj_data))
-print("robot_arm_q_v_tau_data", len(robot_arm_q_v_tau_data))
+    print("kuavo_arm_traj_data : ", len(kuavo_arm_traj_data))
+    print("robot_arm_q_v_tau_data", len(robot_arm_q_v_tau_data))
 
-# 2. 创建3行5列的图表并进行比较
-num_plots = min(len(kuavo_arm_traj_data[0]), len(robot_arm_q_v_tau_data[0]), 15)  # 限制最多只显示15个数据对比
-fig, axs = plt.subplots(3, 5, figsize=(20, 12))
+    # 创建3行5列的图表并进行比较
+    num_plots = min(len(kuavo_arm_traj_data[0]), len(robot_arm_q_v_tau_data[0]), 15)  # 限制最多只显示15个数据对比
+    fig, axs = plt.subplots(3, 5, figsize=(20, 12))
+    fig.suptitle(base_name, fontsize=16)
 
-for i in range(num_plots):
-    kuavo_position = [data[i] for data in kuavo_arm_traj_data]
-    robot_q = [data[i] for data in robot_arm_q_v_tau_data]
-    row = i // 5
-    col = i % 5
-    axs[row, col].plot(kuavo_position, label='/kuavo_arm_traj')
-    axs[row, col].plot(robot_q, label='/robot_arm_q_v_tau')
-    axs[row, col].set_title(f"motor {i+1} state")
-    axs[row, col].legend()
+    for i in range(num_plots):
+        kuavo_position = [data[i] for data in kuavo_arm_traj_data]
+        robot_q = [data[i] for data in robot_arm_q_v_tau_data]
+        row = i // 5
+        col = i % 5
+        axs[row, col].plot(kuavo_position, label='/kuavo_arm_traj')
+        axs[row, col].plot(robot_q, label='/robot_arm_q_v_tau')
+        axs[row, col].set_title(f"motor {i+1} state")
+        axs[row, col].legend()
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+
+    # 保存图片
+    save_path = f"./bag_picture/{base_name}.png"
+    plt.savefig(save_path)
+
+    # 显示图片
+    plt.show()
+
+if __name__ == "__main__":
+    use_rosbag_to_show('./rgb_60hz_bag_file/2024-04-26-17-59-42.bag')
